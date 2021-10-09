7503cb752cf4e3bede813ddb371969019f8e4b64
MittaniK1 <m.keichi16256@gmail.com>
2021-10-09 23:43:44 +0900

    [add]電子レンジ

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] Vector3 direction=new Vector3(-1,0,0);
-    [SerializeField] float speed=1.0f;
+    [SerializeField] Vector3 direction = new Vector3(-1, 0, 0);
+    [SerializeField] float speed = 1.0f;
+
     void Update()
     {
-        transform.position+=new Vector3(direction.x*speed, direction.y*speed, direction.z*speed);
+        transform.position +=
+            new Vector3(direction.x * speed, direction.y * speed, direction.z * speed) * Time.deltaTime;
     }
 }
